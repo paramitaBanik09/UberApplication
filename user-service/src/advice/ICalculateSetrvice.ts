c3b14babfc0fa2eae163f5fd796c23ecf1b781ce
paramitaBanik09 <banikparamita90@gmail.com>
2025-03-15 11:11:41 +0530

    marge conflict

--- user-service/src/advice/ICalculateSetrvice.ts
+++
@@ -1,6 +0,0 @@
-import { VehicleType } from "../types";
-
-export interface ICalculateService {
-  calculateDistanceInKm: (source: [number, number], destination: [number, number]) => number;
-  calculateFare: (distance: number,vehicleType: VehicleType) => number;
-}
